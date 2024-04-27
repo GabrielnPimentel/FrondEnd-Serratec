2a762a3283d680e581b4e2b3e25c1304dee9d18e
GabrielnPimentel <gabrielnpimentel@hotmail.com>
2024-04-27 01:57:08 -0300

    feat: adição do modo escuro e correção no formulário

---
+++ FrontEndIndividual/script.js
@@ -0,0 +1,30 @@
+function toggleMode() {
+    const html = document.documentElement;
+    html.classList.toggle("light");
+
+    
+    const header = document.querySelector("#header .header_content h3");
+    
+    
+    const opcoes = document.querySelector("#opcoes");
+    
+    const img = document.querySelector("#about .about_left img");
+    const about = document.querySelector("#about .about_container h2");
+
+    const footer = document.querySelector("#footer .footer_container h3");
+
+    if (html.classList.contains('light')) {
+        img.setAttribute('src', './src/assets/avatar-light.png');
+        header.textContent = "Seja bem vindo! Esse projeto é uma dedicatória a franquia Fallout, não visando fins lucrativos";
+        footer.textContent = "Desenvolvido por Gabriel Nogueira Pimentel - Turma 18";
+        about.textContent = "Sobre o desenvolvedor";
+        opcoes.classList.add("hidden");
+    } else {
+        img.setAttribute('src', './src/assets/vaultBoyIcon.png');
+        header.textContent = "Bem-vindo à Vault-Tec, pioneira em soluções de abrigo e sobrevivência pós-apocalíptica desde 2054. Como líder global em tecnologia de bunkers e abrigos subterrâneos, a Vault-Tec tem o compromisso de garantir a segurança e o bem-estar dos habitantes do mundo pós-guerra.";
+        footer.textContent = "Desenvolvido por Vault-Tec"
+        about.textContent = "Sobre a empresa";
+        opcoes.classList.remove("hidden");
+
+    }
+}
