1e47b0fe88723983ff76e5416a7df2dd4d21bf55
GabrielnPimentel <gabrielnpimentel@hotmail.com>
2024-04-27 22:25:19 -0300

    fix: header e sobre com fundo correto

--- FrontEndIndividual/script.js
+++ FrontEndIndividual/script.js
@@ -12,6 +12,7 @@ function toggleMode() {
     const about = document.querySelector("#about .about_container h2");
 
     const footer = document.querySelector("#footer .footer_container h3");
+    const player = document.querySelector("#player");
 
     if (html.classList.contains('light')) {
         img.setAttribute('src', './src/assets/avatar-light.png');
@@ -19,12 +20,14 @@ function toggleMode() {
         footer.textContent = "Desenvolvido por Gabriel Nogueira Pimentel - Turma 18";
         about.textContent = "Sobre o desenvolvedor";
         opcoes.classList.add("hidden");
+        player.classList.add("hidden");
     } else {
         img.setAttribute('src', './src/assets/vaultBoyIcon.png');
         header.textContent = "Bem-vindo à Vault-Tec, pioneira em soluções de abrigo e sobrevivência pós-apocalíptica desde 2054. Como líder global em tecnologia de bunkers e abrigos subterrâneos, a Vault-Tec tem o compromisso de garantir a segurança e o bem-estar dos habitantes do mundo pós-guerra.";
         footer.textContent = "Desenvolvido por Vault-Tec"
         about.textContent = "Sobre a empresa";
         opcoes.classList.remove("hidden");
+        player.classList.add("hidden")
 
     }
 }
